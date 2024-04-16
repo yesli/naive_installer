990d182296fbdcda1c73ca0e9bf1a2624d508606
yesli <yesli2011@gmail.com>
2024-04-16 14:17:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,16 @@ yellow='\033[0;33m'
 plain='\033[0m'
 
 cd /usr/local/
+if [[ -e /usr/local/caddy/ ]]; then
+        rm /usr/local/caddy/ -rf
+fi
 wget -N --no-check-certificate -O /usr/local/caddy2-naive-linux-arm64.tar.gz https://gitlab.com/rwkgyg/naiveproxy-yg/-/raw/main/caddy2-naive-linux-arm64.tar.gz
 if [[ $? -ne 0 ]]; then
   echo -e "${red}下载失败，请确保你的服务器正常${plain}"
   exit 1
 fi
-rm -r /usr/local/caddy*
 tar -C /usr/local/caddy -zxf /usr/local/caddy2-naive-linux-arm64.tar.gz
+rm caddy2-naive-linux-arm64.tar.gz -f
 read -p "请设置您的域名:" config_domain
 echo -e "${yellow}您的域名将设定为:${config_domain}${plain}"
 read -p "请设置您的监听端口:" config_port
@@ -28,3 +31,18 @@ echo -e "${yellow}您的密码为:${config_pass}${plain}"
 read -p "请设置伪装网站（不带http/https):" config_fake
 echo -e "${yellow}伪装网站为:${config_fake}${plain}"
 echo -e "${green}正在生成配置文件！${plain}"
+cd caddy
+echo -e "{\n\torder forward_proxy before file_server\n}" >> Caddyfile
+echo -e ":${config_port}, ${config_domain} {" >> Caddyfile
+echo -e "\ttls ${config_cert} ${config_key}" >> Caddyfile
+echo -e "\tforward_proxy {" >> Caddyfile
+echo -e "\t\tbasic_auth ${config_user} ${config_pass}" >> Caddyfile
+echo -e "\t\thide_ip" >> Caddyfile
+echo -e "\t\thide_via" >> Caddyfile
+echo -e "\t\tprobe_resistance" >> Caddyfile
+echo -e "\t}" >> Caddyfile
+echo -e "\treverse_proxy https://${config_fake} {" >> Caddyfile
+echo -e "\t\theader_up  Host  { upstream_hostport }" >> Caddyfile
+echo -e "\t\theader_up  X-Forwarded-Host  { host }" >> Caddyfile
+echo -e "\t}" >> Caddyfile
+echo -e "}" >> Caddyfile
