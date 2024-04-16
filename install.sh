413fdcc0f521c02d4a6c549bc326d0eb3eaf70fc
yesli <yesli2011@gmail.com>
2024-04-16 13:39:56 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+wget -P ~ https://gitlab.com/rwkgyg/naiveproxy-yg/-/raw/main/caddy2-naive-linux-arm64.tar.gz
